3b703c5b4684331aac867ba8db39fac4bd4dfc40
PrynsTag <201812311@fit.edu.ph>
2021-04-07 19:49:01 +0800

    Change position of buttons.

--- app/src/main/java/com/example/bookstore/LoginFragment.kt
+++ app/src/main/java/com/example/bookstore/LoginFragment.kt
@@ -3,30 +3,28 @@ package com.example.bookstore
 
 import android.os.Bundle
 import android.text.TextUtils
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment(), View.OnClickListener {
-
     lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,16 +34,15 @@ class LoginFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v!!.id){
+        when (v!!.id) {
             R.id.next_btn -> {
-                if(!TextUtils.isEmpty(input_username.text.toString())){
+                if (!TextUtils.isEmpty(input_username.text.toString())) {
                     val bundle = bundleOf("recipient" to input_username.text.toString())
                     navController.navigate(
                         R.id.action_LoginFragment_to_BookStoreFragment,
                         bundle
                     )
-                }
-                else{
+                } else {
                     Toast.makeText(activity, "Enter a name", Toast.LENGTH_SHORT).show()
                 }
             }
