b20d52834c33006e660a77008015a56a4e7b7981
PrynsTag <201812311@fit.edu.ph>
2021-04-09 03:33:15 +0800

    Fix book table to include user_id of user.

--- app/src/main/java/com/example/bookstore/DatabaseHelper.kt
+++ app/src/main/java/com/example/bookstore/DatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.bookstore
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
@@ -14,18 +15,19 @@ class DatabaseHelper(context: Context) :
         )
         db.execSQL(
             "CREATE TABLE book(book_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    "book_name TEXT NOT NULL, book_author NOT NULL, book_image NOT NULL, " +
-                    "book_pages INTEGER, user_id INTEGER NOT NULL, " +
+                    "book_name TEXT NOT NULL, book_author TEXT NOT NULL, book_image TEXT NULL, " +
+                    "book_pages TEXT, user_id INTEGER NOT NULL, " +
                     "FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE ON UPDATE CASCADE)"
         )
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS user")
+        db.execSQL("DROP TABLE IF EXISTS book")
+        onCreate(db)
     }
 
     fun insertUserData(
-//        context: Context,
         name: String,
         email: String,
         username: String,
@@ -41,50 +43,46 @@ class DatabaseHelper(context: Context) :
             values.put("user_password", password)
             db.insert("user", null, values)
             db.close()
-//            return Toast.makeText(context, "Successful", Toast.LENGTH_SHORT).show()
         }
 
         db.close()
-//        return Toast.makeText(context, "User Exist", Toast.LENGTH_SHORT).show()
     }
 
+    @SuppressLint("Recycle")
     fun insertBookData(
-//        context: Context,
         title: String,
         author: String,
-        image: String,
-        pages: Int,
-        user_id: Int
+//        image: String,
+        pages: String,
+        username: String,
+        password: String
     ) {
         val db: SQLiteDatabase = writableDatabase
-
+        val query = "SELECT user_id FROM user " +
+                "WHERE user_username = '$username' AND user_password = '$password'"
+        val cursor = db.rawQuery(query, null)
         val values = ContentValues()
-        if (!isExists(title, author, "book")) {
+        if (!isExists("book", title, author)) {
             values.put("book_name", title)
             values.put("book_author", author)
-            values.put("book_image", image)
+//            values.put("book_image", image)
             values.put("book_pages", pages)
-            values.put("user_id", user_id)
+            values.put("user_id", cursor.moveToFirst())
             db.insert("book", null, values)
             db.close()
-//            Toast.makeText(context, "Successful", Toast.LENGTH_SHORT).show()
-//            return
         }
-
-        db.close()
-//        Toast.makeText(context, "User Exist", Toast.LENGTH_SHORT).show()
-        return
     }
 
     fun isExists(dbName: String, username: String, password: String): Boolean {
         val db: SQLiteDatabase = readableDatabase
         val query =
             if (dbName == "user") {
-            "SELECT * FROM $dbName " +
-                    "WHERE user_username = '$username' AND user_password = '$password'" }
-            else {
                 "SELECT * FROM $dbName " +
-                        "WHERE book_name = '$username' AND book_author = '$password'" }
+                        "WHERE user_username = '$username' AND user_password = '$password'"
+            } else {
+                "SELECT * FROM $dbName " +
+                        "WHERE book_name = '$username' AND book_author = '$password'"
+            }
 
         val cursor = db.rawQuery(query, null)
 
@@ -98,7 +96,7 @@ class DatabaseHelper(context: Context) :
     }
 
     companion object {
-        internal const val DATABASE_NAME = "user"
+        internal const val DATABASE_NAME = "BookStore"
         internal const val DATABASE_VERSION = 1
     }
 }
