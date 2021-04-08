ec4a538782f03bd9d7dfa480bd3d23680ffa2827
PrynsTag <201812311@fit.edu.ph>
2021-04-08 16:06:37 +0800

    revert BookStoreFragment.kt and fragment_book_store.xml to working code.

--- app/src/main/java/com/example/bookstore/BookStoreFragment.kt
+++ app/src/main/java/com/example/bookstore/BookStoreFragment.kt
@@ -2,85 +2,64 @@ package com.example.bookstore
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_book_store.*
-import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
-import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 
-class BookStoreFragment : AppCompatActivity() {
 
+class BookStoreFragment : Fragment(), View.OnClickListener {
     lateinit var navController: NavController
     lateinit var recipient: String
-
-    class ExampleViewHolder(itemView: View) :
-        RecyclerView.ViewHolder(itemView) {   //Example sa youtube
-//        val imageview: ImageView = itemView.
-    }
+    lateinit var message: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_book_store)
+        recipient = arguments!!.getString("recipient").toString()
+    }
 
-        //  API Code
-        val title = mutableListOf<String>() //  API Book Title
-        val author = mutableListOf<String>() //  API Book Author
-        val numpages = mutableListOf<String>() //  API Number of Pages
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_book_store, container, false)
+    }
 
-        val textView_Result = findViewById<TextView>(R.id.txv_result)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        message = "Sending money to $recipient"
 
-// ...
+        view.findViewById<Button>(R.id.send_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
+        view.findViewById<TextView>(R.id.recipient).text = message
+    }
 
-// Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-        val url = "http:/10.0.2.2/FinalProject/"
-
-// Request a string response from the provided URL.
-        val stringRequest = StringRequest(Request.Method.GET, url,
-            { response ->
-//                textView.text = response.toString()
-                Log.i("test", "test")
-                try {
-                    val json_obj: JSONObject = JSONObject(response.toString())
-                    val arr_items: JSONArray = json_obj.getJSONArray("book")
-                    for (i in 0..arr_items.length() - 1) {
-                        val item: JSONObject = arr_items.getJSONObject(i)
-                        val item_title = item.getString("title")    //  Get Book Title
-                        val item_author = item.getString("authors")[0]  //  Get Author
-                        val item_numpage = item.getString("pageCount")  //  Get Book Number of Page
-
-                        title.add(item_title)   // Add item to mutablelist title
-                        author.add(item_author.toString())
-                        numpages.add(item_numpage)  //  Add num page to num page mutable list
-                    }
-
-//                    textView.text = response.toString()
-                    textView_Result.text = title[0]
-
-                } catch (e: JSONException) {
-                    e.printStackTrace()
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.send_btn -> {
+                if (!TextUtils.isEmpty(input_amount.text.toString())) {
+                    val amount = input_amount.text.toString()
+                    val bundle = bundleOf(
+                        "recipient" to recipient,
+                        "amount" to amount
+                    )
+                    navController.navigate(
+                        R.id.action_BookStoreFragment_to_CartFragment,
+                        bundle
+                    )
+                } else {
+                    Toast.makeText(activity, "Enter an amount", Toast.LENGTH_SHORT).show()
                 }
-            },
-            { textView_Result.text = "That didn't work!" })
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest)
+            }
+            R.id.cancel_btn -> activity!!.onBackPressed()
+        }
     }
 }
