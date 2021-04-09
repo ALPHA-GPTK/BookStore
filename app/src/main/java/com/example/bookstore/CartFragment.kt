357b815ebc8f57d953a9e6de6c9baeaeb71722e8
PrynsTag <201812311@fit.edu.ph>
2021-04-09 15:32:31 +0800

    Added recycler view to display data

--- app/src/main/java/com/example/bookstore/CartFragment.kt
+++ app/src/main/java/com/example/bookstore/CartFragment.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_cart.*
 
 class CartFragment : Fragment() {
 
     lateinit var username: String
     lateinit var password: String
+    lateinit var handler: DatabaseHelper
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,15 +27,18 @@ class CartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val amount = money!!
-        val confirmationMessage = "You have are $$username to $password"
-//        view.findViewById<TextView>(R.id.confirmation_message).text = confirmationMessage
-        view.findViewById<TextView>(R.id.confirmation_message).text = confirmationMessage
+        val layoutManager = LinearLayoutManager(activity!!)
+        recycler_cart_view.layoutManager = layoutManager
+
+        handler = DatabaseHelper(activity!!)
+        val (title, author, imageUrl, numPages) = handler.getBookData(username, password)
+
+        adapter = RecyclerAdapter(activity!!, title, author, numPages, imageUrl, username, password)
+        recycler_cart_view.adapter = adapter
     }
 }
