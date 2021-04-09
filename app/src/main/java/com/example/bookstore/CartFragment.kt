a2c75499a6b4508ee804ff2f95005cf21d392a84
Katherine Mariñas <55967594+Kathorsiii@users.noreply.github.com>
2021-04-09 16:05:50 +0800

    Update CartFragment.kt

--- app/src/main/java/com/example/bookstore/CartFragment.kt
+++ app/src/main/java/com/example/bookstore/CartFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_cart.*
@@ -15,6 +17,7 @@ class CartFragment : Fragment() {
     lateinit var username: String
     lateinit var password: String
     lateinit var handler: DatabaseHelper
+    lateinit var navController: NavController
     private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +35,14 @@ class CartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
         val layoutManager = LinearLayoutManager(activity!!)
         recycler_cart_view.layoutManager = layoutManager
 
         handler = DatabaseHelper(activity!!)
         val (title, author, imageUrl, numPages) = handler.getBookData(username, password)
 
-        adapter = RecyclerAdapter(activity!!, title, author, numPages, imageUrl, username, password)
+        adapter = RecyclerAdapter(activity!!, title, author, numPages, imageUrl, username, password, navController)
         recycler_cart_view.adapter = adapter
     }
 }
