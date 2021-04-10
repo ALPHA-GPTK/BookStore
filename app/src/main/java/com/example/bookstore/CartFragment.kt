ecafd8c6c4f61925159fa72064becdb442165bb9
PrynsTag <201812311@fit.edu.ph>
2021-04-10 19:32:07 +0800

    Calculate total price.

--- app/src/main/java/com/example/bookstore/CartFragment.kt
+++ app/src/main/java/com/example/bookstore/CartFragment.kt
@@ -5,22 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.card_item.view.*
 import kotlinx.android.synthetic.main.fragment_cart.*
 
 class CartFragment : Fragment() {
 
-    lateinit var username: String
-    lateinit var password: String
-    lateinit var handler: DatabaseHelper
-    lateinit var navController: NavController
-    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+    private lateinit var username: String
+    private lateinit var password: String
+
+    private lateinit var handler: DatabaseHelper
+    private lateinit var navController: NavController
+    private lateinit var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,17 +37,10 @@ class CartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-        val container = activity!!.findViewById<FrameLayout>(R.id.container)
-        val rootView = LayoutInflater.from(activity!!).inflate(R.layout.card_item, container, false)
-
-        // Set Visibility of Button in CartFragment to GONE ERROR!
-        rootView.btn_add.visibility = View.GONE // NOT FIXED
-
-        val layoutManager = LinearLayoutManager(activity!!)
-        recycler_cart_view.layoutManager = layoutManager
-
         handler = DatabaseHelper(activity!!)
+
         val (title, author, numPages, imageUrl, price) = handler.getBookData(username, password)
+        txv_totalAmount.text = price.sum().toString()
 
         adapter = RecyclerAdapter(
             activity!!,
@@ -63,5 +55,7 @@ class CartFragment : Fragment() {
             R.layout.card_delete
         )
         recycler_cart_view.adapter = adapter
+        recycler_cart_view.layoutManager = LinearLayoutManager(activity!!)
+        recycler_cart_view.setHasFixedSize(true)
     }
 }
